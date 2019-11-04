06b885ae197b484a117ea46caaf7c49f4a940f50
Alex Lee <hyunho98@gmail.com>
2019-11-04 08:03:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -96,4 +96,15 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
+  index = 0
+  total = 0
+
+  while index < cart.length do
+    total += cart[index][:price] * cart[index][:count]
+  end
+
+  total
 end
