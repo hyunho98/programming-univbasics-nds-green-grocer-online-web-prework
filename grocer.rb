a70acb953fa2eee139d83d494058fb1cac161479
Alex Lee <hyunho98@gmail.com>
2019-11-04 07:33:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -54,12 +54,7 @@ def apply_coupons(cart, coupons)
     couponIndex = 0
     while couponIndex < coupons.length do
       if cart[cartIndex][:item] == coupons[couponIndex][:item] && cart[cartIndex][:count] >= coupons[couponIndex][:num]
-        cart << {
-          item: coupons[couponIndex][:item] + " W/ COUPON",
-          price: coupons[couponIndex][:cost] / coupons[couponIndex][:num],
-          clearance: cart[cartIndex][:clearance],
-          count: coupons[couponIndex][:num]
-        }
+        cart << {item: coupons[couponIndex][:item] + " W/ COUPON", price: coupons[couponIndex][:cost] / coupons[couponIndex][:num], clearance: cart[cartIndex][:clearance], count: coupons[couponIndex][:num]}
         cart[cartIndex][:count] -= coupons[couponIndex][:num]
       end
       couponIndex += 1
